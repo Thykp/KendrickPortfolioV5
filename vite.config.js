efdc277005ae71f2cc3ab9cad402288c6786d4cb
Thykp <101046365+Thykp@users.noreply.github.com>
2024-05-26 18:48:16 +0800

    push

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/KendrickPortfolio/",
+  base: "/KendrickPortfolioV5/",
   plugins: [react()],
 })
